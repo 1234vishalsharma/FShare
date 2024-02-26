7426eebd3e438a2fd35b69b4d7d2fcac07460fdf
Vishal Sharma <vishalsharma15122003@gmail.com>
2024-02-26 16:51:27 +0530

    Final commit

---
+++ app/(dashboard)/(routes)/Files/page.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+
+function Files() {
+  return (
+    <div className='flex flec-col'>
+      This is File section
+    </div>
+  )
+}
+
+export default Files
---
+++ app/(dashboard)/(routes)/file-preview/[fileId]/_components/Fileinfo.js
@@ -0,0 +1,45 @@
+import { CopyIcon } from 'lucide-react';
+import React from 'react'
+import { useState } from 'react';
+
+const Fileinfo = ({file, OnPasswordSaved}) => {
+
+  const [password,setPassword] = useState('');
+  
+  const handleChange = (event) => {
+    setPassword(event.target.value)  
+  }
+ 
+
+  return (
+    <div className='flex flex-col gap-4 p-8 w-full'>
+        <label htmlFor="UserEmail" className="block text-lg font-medium text-gray-700"> File Url </label>
+      <div className='w-full flex  rounded-md border border-gray-200'>
+
+        <input
+          value={file?.ShortUrl}
+          type="url"
+          placeholder="http://localhost:3000/"
+          className="mt-1 w-5/6 shadow-sm sm:text-md p-4"
+        />
+        
+        <CopyIcon size={32} className='flex justify-center items-center h-full ml-4'/>
+      </div>
+    
+
+      <div className='flex gap-4 w-full items-center justify-between'>
+        <label htmlFor="UserEmail" className="block text-lg font-medium text-gray-700"> Add Password </label>
+          <input
+            onChange={handleChange}
+            type="url"
+            placeholder="eg: 123@1!^%abAC (Optional)"
+            className="mt-1 w-full border rounded-md border-gray-200 shadow-sm sm:text-md p-4"
+          />
+        <button className='border-black border bg-slate-500 text-white p-4 rounded-lg' onClick={()=>OnPasswordSaved(password)}>
+          Add Password</button>
+      </div>
+      <h2 className='text-center mt-5 font-bold text-slate-400 text-2xl'>Share the provided link to share the file.</h2>
+    </div>
+  )}
+
+export default Fileinfo
